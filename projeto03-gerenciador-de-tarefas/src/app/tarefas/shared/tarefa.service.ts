10013093c1e9f4dee0cb690a57586cd1963efbfa
Bruno <brunocosta.1991@hotmail.com>
2022-02-02 10:15:42 -0300

    Gerenciamento de tarefas parte 2

--- projeto03-gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
+++ projeto03-gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+
 import { Tarefa } from './';
 
 @Injectable({
@@ -23,5 +24,31 @@ export class TarefaService {
   buscarPorId(id: number): Tarefa {
     const tarefas: Tarefa[] = this.listarTodos();
     return tarefas.find(tarefa => tarefa.id === id);
+  }
+
+  atualizar(tarefa: Tarefa): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (tarefa.id = obj.id) {
+        objs[index] = tarefa;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  remover(id: number): void {
+    let tarefas: Tarefa[] = this.listarTodos();
+    tarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  alterarStatus(id: number): void {
+    const tarefas: Tarefa[] = this.listarTodos();
+    tarefas.forEach((obj, index, objs) => {
+      if (id = obj.id) {
+        objs[index].concluida = !obj.concluida;
+      }
+    });
+    localStorage['tarefas'] = JSON.stringify(tarefas);
   }
 }
