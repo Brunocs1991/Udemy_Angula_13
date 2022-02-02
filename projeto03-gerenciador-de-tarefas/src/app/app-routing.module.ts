c1b84ebc9193f92a6649703ddff357f17c337ef6
Bruno <brunocosta.1991@hotmail.com>
2022-02-02 13:09:16 -0300

    registrando rotas e configurando html listagem

--- projeto03-gerenciador-de-tarefas/src/app/app-routing.module.ts
+++ projeto03-gerenciador-de-tarefas/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { TarefasRoutes } from './tarefas';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 export const routes: Routes = [
-
+  {
+    path: '',
+    redirectTo: 'tarefas/listar',
+    pathMatch: 'full'
+  },
+  ...TarefasRoutes
 ];
 
 @NgModule({
