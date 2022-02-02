66e499052ed4fe17f5406a6eb1d99d2346bc18aa
Bruno <brunocosta.1991@hotmail.com>
2022-02-02 19:42:22 -0300

    Cadastro de tarefas

--- projeto03-gerenciador-de-tarefas/src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
+++ projeto03-gerenciador-de-tarefas/src/app/tarefas/cadastrar/cadastrar-tarefa.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Tarefa, TarefaService } from '../shared';
 
 @Component({
   selector: 'app-cadastrar-tarefa',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarTarefaComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTarefa', { static: true }) formTarefa: NgForm;
+
+  tarefa: Tarefa
+
+  constructor(private tarefaService: TarefaService, private router: Router) { }
 
   ngOnInit(): void {
+    this.tarefa = new Tarefa();
+  }
+
+  cadastrar(): void {
+    if (this.formTarefa.form.valid) {
+      this.tarefaService.cadastrar(this.tarefa);
+      this.router.navigate(["/tarefas"]);
+    }
   }
 
 }
