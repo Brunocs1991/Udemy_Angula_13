3fbd63a4ae101fbc8b8e2ac8899afe21efa7a60a
Bruno <brunocosta.1991@hotmail.com>
2022-02-02 10:07:51 -0300

    Gerenciamento de tarefas parte 1

---
+++ projeto03-gerenciador-de-tarefas/src/app/tarefas/shared/tarefa.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { Tarefa } from './';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TarefaService {
+
+  constructor() { }
+
+  listarTodos(): Tarefa[] {
+    const tarefas = localStorage['tarefas'];
+    return tarefas ? JSON.parse(tarefas) : [];
+  }
+
+  cadastrar(tarefa: Tarefa): void {
+    const tarefas = this.listarTodos();
+    tarefa.id = new Date().getTime();
+    tarefas.push(tarefa);
+    localStorage['tarefas'] = JSON.stringify(tarefas);
+  }
+
+  buscarPorId(id: number): Tarefa {
+    const tarefas: Tarefa[] = this.listarTodos();
+    return tarefas.find(tarefa => tarefa.id === id);
+  }
+}
--- projeto03-gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
+++ projeto03-gerenciador-de-tarefas/src/app/tarefas/tarefas.module.ts
@@ -1,3 +1,4 @@
+import { TarefaService } from './shared';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -6,7 +7,10 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+  ],
+  providers: [
+    TarefaService
   ]
 })
 export class TarefasModule { }
