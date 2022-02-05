c66f56f4070db245b1b7c0d6e32534109302ac5a
Bruno <brunocosta.1991@hotmail.com>
2022-02-05 18:29:14 -0300

    modulo dashboard

--- projeto06-dashboard/src/app/app.module.ts
+++ projeto06-dashboard/src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DashboardModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
