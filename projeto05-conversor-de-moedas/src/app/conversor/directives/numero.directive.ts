08bf73d57c9fa8ff08550d8b920e6373232e8054
Bruno <brunocosta.1991@hotmail.com>
2022-02-05 16:34:02 -0300

    directive number

--- projeto05-conversor-de-moedas/src/app/conversor/directives/numero.directive.ts
+++ projeto05-conversor-de-moedas/src/app/conversor/directives/numero.directive.ts
@@ -1,10 +1,70 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Directive({
-  selector: '[appNumero]'
+  selector: '[numero]',
+  providers: [{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: NumeroDirective,
+    multi: true
+  }]
 })
-export class NumeroDirective {
+export class NumeroDirective implements ControlValueAccessor{
 
-  constructor() { }
+
+  onTouched: any;
+  onChange: any;
+
+  constructor(private el: ElementRef) {}
+
+  /**
+   * Implementa evento de keyup para o elemento da diretiva.
+   *
+   * @param any $event
+   */
+  @HostListener('keyup', ['$event'])
+  onKeyUp($event: any) {
+    let valor = $event.target.value;
+    let posDecimais = valor.indexOf('.');
+
+    valor = valor.replace(/[\D]/g, '');
+
+    if (posDecimais > 0) {
+      valor = valor.substr(0, posDecimais) + '.' +
+        valor.substr(posDecimais);
+    }
+
+    $event.target.value = valor;
+    this.onChange(valor);
+  }
+
+  /**
+   * Registra função a ser chamada para atualizar
+   * valor na model.
+   *
+   * @param any fn
+   */
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+
+  /**
+   * Registra função a ser chamada para atualizar
+   * valor na model para evento touched.
+   *
+   * @param any fn
+   */
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  /**
+   * Obtém o valor contido na model.
+   *
+   * @param any value
+   */
+  writeValue(value: any): void {
+  	this.el.nativeElement.value = value;
+  }
 
 }
