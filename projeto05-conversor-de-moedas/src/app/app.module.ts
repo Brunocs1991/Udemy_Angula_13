3b9cbb73939f00129a7ab426c62ba4cdb83f6628
Bruno <brunocosta.1991@hotmail.com>
2022-02-05 12:25:13 -0300

    module conversor

--- projeto05-conversor-de-moedas/src/app/app.module.ts
+++ projeto05-conversor-de-moedas/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ConversorModule } from './conversor/conversor.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ConversorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
