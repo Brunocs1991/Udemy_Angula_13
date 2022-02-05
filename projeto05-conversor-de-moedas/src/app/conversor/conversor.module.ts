e945320b86e9c90c5e51ae36c1e3a8f3bef4ab9d
Bruno <brunocosta.1991@hotmail.com>
2022-02-05 16:57:27 -0300

    finalizando

--- projeto05-conversor-de-moedas/src/app/conversor/conversor.module.ts
+++ projeto05-conversor-de-moedas/src/app/conversor/conversor.module.ts
@@ -6,12 +6,16 @@ import { ConversorComponent } from './components/conversor.component';
 import { ConversorService } from './services/conversor.service';
 import { MoedaService } from './services/moeda.service';
 import { NumeroDirective } from './directives/numero.directive';
+import { ModalCotacaoComponent } from './utils/modal-cotacao.component';
+import { DataBrPipe } from './pipes/data-br.pipe';
 
 
 @NgModule({
   declarations: [
     ConversorComponent,
-    NumeroDirective
+    NumeroDirective,
+    ModalCotacaoComponent,
+    DataBrPipe
   ],
   imports: [
     CommonModule,
