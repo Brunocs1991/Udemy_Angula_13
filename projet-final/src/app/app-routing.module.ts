321f401ba2ddc601c12f923f83fb9924421b4bec
Bruno <brunocosta.1991@hotmail.com>
2022-02-08 18:39:02 -0300

    ntegrando e configurando dashboard

--- projet-final/src/app/app-routing.module.ts
+++ projet-final/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { DashboardRoutes } from './dashboard/dashboard-routing.module';
+
+
 export const routes: Routes = [
   {
     path: '',
     redirectTo: '/dashboard',
     pathMatch: 'full'
-  }
+  },
+  ...DashboardRoutes
 ];
 
 @NgModule({
